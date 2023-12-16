272467d371cb764188303bbde01fda8d2543d8d5
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2023-12-16 16:09:31 +0900

    feat: update router

--- src/main.go
+++ src/main.go
@@ -14,6 +14,6 @@ func main() {
 		log.Fatal("環境変数を設定してください")
 	}
 
-	e := router.New()
-	e.Logger.Fatal(e.Start(":" + os.Getenv("WEB_PORT")))
+	r := router.New()
+	r.Logger.Fatal(r.Start(":" + os.Getenv("WEB_PORT")))
 }
--- src/router/main.router.go
+++ src/router/main.router.go
@@ -2,9 +2,13 @@ package router
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func New() *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	return e
 }
