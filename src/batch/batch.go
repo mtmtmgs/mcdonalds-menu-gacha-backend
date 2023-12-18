bfcd6b51744306c339872c1311a36ef07d96d2a8
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2023-12-19 03:43:14 +0900

    style: update file name

---
+++ src/batch/batch.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hm-mtmtmgs/mcdonalds-menu-gacha-backend/batch/scripts"
+	"github.com/hm-mtmtmgs/mcdonalds-menu-gacha-backend/db"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := db.New()
+	defer db.Close()
+
+	err = scripts.FetchAndRegisterMcdonaldsMenu(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- src/batch/scripts/mcdonalds-menu-scraping.script.go
+++ src/batch/scripts/mcdonalds-menu-scraping.script.go
@@ -1,21 +1,21 @@
-package main
+package scripts
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/uptrace/bun"
 )
 
-func FetchAndRegisterMcdonaldsMenu() error {
+func FetchAndRegisterMcdonaldsMenu(db *bun.DB) error {
 	res, err := http.Get("https://www.mcdonalds.co.jp/menu/burger/")
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return errors.New(fmt.Sprintf("status code error: %d %s", res.StatusCode, res.Status))
+		return fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
@@ -28,5 +28,6 @@ func FetchAndRegisterMcdonaldsMenu() error {
 		price := elem.Find(".product-list-card-price .product-list-card-price-number").Text()
 		fmt.Printf("Review %d: %s %s\n", i, name, price)
 	})
+
 	return nil
 }
