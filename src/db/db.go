ab8d050672bfd6ea8c5f0aa16c72fb2af825d176
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2024-01-03 00:13:15 +0900

    feat: update docker compose db host

--- src/db/db.go
+++ src/db/db.go
@@ -2,6 +2,8 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/uptrace/bun"
@@ -11,9 +13,20 @@ import (
 )
 
 func New() *bun.DB {
-	dsn := os.Getenv("DATABASE_URL")
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
 	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
 	db := bun.NewDB(sqldb, pgdialect.New())
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// .envのBUNDEBUGでデバッグモードを切り替え
 	db.AddQueryHook(bundebug.NewQueryHook(
