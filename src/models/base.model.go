153555d825753f12986edb3956ebfbaf3281479b
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2024-01-02 20:00:13 +0900

    feat: add soft delete column to table base

--- src/models/base.model.go
+++ src/models/base.model.go
@@ -6,4 +6,5 @@ type Base struct {
 	Id        uint      `bun:"id,pk,autoincrement"`
 	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
 	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	DeletedAt time.Time `bun:",soft_delete,nullzero"`
 }
