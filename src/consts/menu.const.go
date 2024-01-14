677fbd041e2cacce466106a63f2a62b71f008408
hm-mtmtmgs <150935913+hm-mtmtmgs@users.noreply.github.com>
2024-01-15 01:56:46 +0900

    refactor: add menu constants file

---
+++ src/consts/menu.const.go
@@ -0,0 +1,18 @@
+package consts
+
+const (
+	Morning = "朝マック"
+	Noon    = "ひるまック"
+	Night   = "夜マック"
+	Regular = "レギュラー"
+)
+
+const (
+	Burger   = "バーガー"
+	Set      = "セット"
+	Side     = "サイドメニュー"
+	Drink    = "ドリンク"
+	HappySet = "ハッピーセット"
+	Dessert  = "スイーツ"
+	McCafe   = "マックカフェ"
+)
